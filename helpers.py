5ca91b3df7aa87b02b929ffb6932ca2ea01cd49a
Manoj <saimanoj@gmail.com>
2026-05-08 12:09:13 +0530

    added a helper file

---
+++ helpers.py
@@ -0,0 +1,19 @@
+def multiply(a,b):
+    return a*b
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
+
+def power(a,b):
+    return a**b
+
+def greet(name):
+    print("Hello")
+    print(f"Welcome {name} to the application")
+    print("Thank you for the visit")
