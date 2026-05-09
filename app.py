e019a380f50db1c6b611d5dc8f0ccfbe95ad2608
Manoj <saimanoj@gmail.com>
2026-05-09 11:11:43 +0530

    Final version

--- app.py
+++ app.py
@@ -8,35 +8,31 @@ n1 = int(st.number_input("Enter the first number :"))
 #n2 = int(input("Enter the Second number :"))
 n2 = int(st.number_input("Enter the second number :"))
 
-st.write("Select the operation to do on the two number")
-st.write("Type 1 - Multiplication")
-st.write("Type 2 - Addition")
-st.write("Type 3 - Subtraction")
-st.write("Type 4 - Power / exponential")
-st.write("Type 5 - divide")
-st.write('Type 6 - Floor division')
+operator = st.selectbox("Operation",['Multiply','Add','Subtraction','Divide','Power','Floor division'])
 
-#operator = int(input("Enter the Option from above :"))
-operator = int(st.number_input("Enter the Option from above :"))
+#a = st.button('click')
+#st.write(a)
 
-if operator == 1:
-    st.write(multiply(n1,n2))
+if st.button("Show Result"):
 
-elif operator == 2:
-    st.write(add(n1,n2))
+    if operator == 'Multiply':
+        st.write(multiply(n1,n2))
 
-elif operator == 3:
-    st.write(sub(n1,n2))
+    elif operator == 'Add':
+        st.write(add(n1,n2))
 
-elif operator == 4:
-    st.write(power(n1,n2))
+    elif operator == 'Subtraction':
+        st.write(sub(n1,n2))
 
-elif operator == 5:
-    st.write(divide(n1,n2))
+    elif operator == 'Power':
+        st.write(power(n1,n2))
 
-elif operator == 6:
-    st.write(floor(n1,n2))
+    elif operator == 'Divide':
+        st.write(divide(n1,n2))
 
-else:
-    st.write("Please select vaild Option")
+    elif operator == 'Floor division':
+        st.write(floor(n1,n2))
+
+    else:
+        st.write("Please select vaild Option")
 
